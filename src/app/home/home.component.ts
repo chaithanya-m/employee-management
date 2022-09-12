efd2252bfeb14b6f92fd88b653ec9e096f046a80
chaithanya <chaithanyamadduru03@gmail.com>
2022-09-12 12:12:18 +0530

    add validations using reactive forms

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dial
 import { Employee } from '../employee';
 import { EmployeeService } from '../employee.service'
 import { first, take } from 'rxjs/operators';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 
 @Component({
@@ -11,33 +12,29 @@ import { first, take } from 'rxjs/operators';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  email: any=null;
-  name: any=null;
-  
   displayedColumns: string[] = ['EMP_ID', 'Name', 'Email', 'Actions'];
   dataSource: Employee[] = [];
 
   constructor(public dialog: MatDialog, private employeeService: EmployeeService) {}
+  
+  ngOnInit(): void {
+    this.getAllemployees();
+  }
 
   openDialog(): void {
     const dialogRef = this.dialog.open(ExampleDialog, {
       width: '300px',
-      data: {name: this.name, email: this.email},
+      data: {name: '', email: ''},
     });
-    dialogRef.afterClosed().subscribe(result => { 
-      if(result.name != null && result.email!=null){
-      const newEmployee: Employee = {
-        name: result.name,
-        email: result.email
-      }
+    dialogRef.afterClosed().subscribe(formGroup => { 
+      const newEmployee: Employee = formGroup.getRawValue();
+      
       this.employeeService.addEmployee(newEmployee).then(
         (response) => {
           this.getAllemployees()
         }, (error)=>{
         console.log(error)
-      });  
-    }   
+      });    
     });
   }
   //get all employeees
@@ -59,22 +56,23 @@ export class HomeComponent implements OnInit {
   }
 
   edit(id:any){
-    this.employeeService.getEmployee(id).snapshotChanges() .subscribe(
+    this.employeeService.getEmployee(id).snapshotChanges().subscribe(
       (data: any) => {
         const employee = data.payload.toJSON();
+        
         const dialogRef = this.dialog.open(ExampleDialog, {
           width: '300px',
           data: {name: employee.name, email: employee.email},
         })
-        dialogRef.afterClosed().pipe(take(1)).subscribe(result => {    
-          if(result == null){
+        dialogRef.afterClosed().pipe(take(1)).subscribe(formGroup => {    
+          if(formGroup == null){
             return
           }
           else{
-            this.employeeService.
-            updateEmployee(result).then(
+            const newEmployee: Employee = formGroup.getRawValue();
+            this.employeeService.updateEmployee(newEmployee).then(
               (response) => {
-               
+                this.dialog.closeAll();
               }, (error)=>{
               console.log(error)
             });       
@@ -88,11 +86,7 @@ export class HomeComponent implements OnInit {
   delete(id:any){
     this.employeeService.deleteEmployee(id) 
     this.getAllemployees()
-  }
-  ngOnInit(): void {
-    this.getAllemployees();
-  }
- 
+  } 
 }
 // dialog class 
 @Component({
@@ -100,13 +94,26 @@ export class HomeComponent implements OnInit {
   templateUrl: 'emp-detail-dialog.html',
 })
 export class ExampleDialog {
+  formGroup: FormGroup;
+  
   constructor(
+    private formBuilder: FormBuilder, 
     public dialogRef: MatDialogRef<ExampleDialog>,
     @Inject(MAT_DIALOG_DATA) public data: Employee,
-  ) {}
-  Employee={
-  name:"hello"
+  ) {
+
+    let emailregex: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    this.formGroup = this.formBuilder.group({
+      'name': [data.name, Validators.required],
+      'email': [data.email, [Validators.required, Validators.pattern(emailregex)]]
+    });
   }
+
+  getErrorEmail() {
+    return this.formGroup!.get('email')!.hasError('required') ? 'Email is required' :
+      this.formGroup!.get('email')!.hasError('pattern') ? 'Not a valid emailaddress' : '';
+  }
+
   onNoClick(): void {
     this.dialogRef.close();
   }
